42e071897948ed4ff58e4b21618fb4a2dcb17584
atshaw1994 <atshaw1994@gmail.com>
2026-08-25 00:03:42 -0400

    Implement android UI

---
+++ Apps/UI/Avalonia/PCMHammerAvalonia/Views/LogTypeMenuExpander.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PCMHammerAvalonia.Views
+{
+    public partial class LogTypeMenuExpander : UserControl
+    {
+        public LogTypeMenuExpander()
+        {
+            InitializeComponent();
+        }
+    }
+}
