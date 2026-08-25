bd0bfa6619b9a0c3c2b3afbd0677377d3f078041
atshaw1994 <atshaw1994@gmail.com>
2026-08-25 08:00:10 -0400

    Redesign android interface

---
+++ Apps/UI/Avalonia/PCMHammerAvalonia/ViewModels/SideBarMenuViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PCMHammerAvalonia.ViewModels;
+
+namespace PCMHammerAvalonia.ViewModels;
+
+public partial class SideBarMenuViewModel : ViewModelBase
+{
+    
+}
